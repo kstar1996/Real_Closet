ddd57127d7652d81940fa869076c9df5203c9534
eujin <kstar1996@gmail.com>
2019-03-20 23:58:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/eujin/real_closet/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.eujin.real_closet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int count = 5;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name=(EditText)findViewById(R.id.etName);
+        Password=(EditText)findViewById((R.id.etPassword));
+        Info=(TextView)findViewById(R.id.tvInfo);
+        Login=(Button)findViewById(R.id.btnLogin);
+
+        Info.setText("# of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if ((userName=="Admin") && (userPassword=="12345")){
+            Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+            //start and destination
+
+            startActivity(intent);
+        }
+
+        else{
+            count--; //after 5 times button is disabled
+
+            Info.setText("# of attemps remaining: " + String.valueOf(count));
+
+            if(count==0){
+                Login.setEnabled(false); //disable button
+            }
+        }
+
+
+    }
+}
