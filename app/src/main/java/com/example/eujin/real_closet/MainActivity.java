74d220dac4df5da0a75f1abd107e2185f3064613
eujin <kstar1996@gmail.com>
2019-04-11 01:40:47 +0900

    Some changes!!!

--- app/src/main/java/com/example/eujin/real_closet/MainActivity.java
+++ app/src/main/java/com/example/eujin/real_closet/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText Name;
--- app/src/main/java/com/example/eujin/real_closet/MenuActivity.java
+++ app/src/main/java/com/example/eujin/real_closet/MenuActivity.java
@@ -6,8 +6,10 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,8 +36,7 @@ public class MenuActivity extends AppCompatActivity {
     private Uri filePath;
 
 
-
-
+    private final int PICK_IMAGE_REQUEST = 71;
 
 
     private static final int PICK_FROM_GALLERY;
@@ -53,10 +55,9 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         //init view
-
-
         btn_camera = (ImageButton) findViewById(R.id.btnCamera);
         btn_gallery = (ImageButton) findViewById(R.id.btnGallery);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,14 +66,34 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
-
-        btn_gallery.setOnClickListener(new View.OnClickListener() {
+        /*btn_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 goToGallery();
             }
         });
+        */
+
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+
+
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    private void chooseImage(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -96,10 +117,10 @@ public class MenuActivity extends AppCompatActivity {
             Cursor cursor = null;
 
             try {
-                String[] proj = { MediaStore.Images.Media.DATA };
+                String[] project = { MediaStore.Images.Media.DATA };
 
                 assert photoUri != null;
-                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+                cursor = getContentResolver().query(photoUri, project, null, null, null);
 
                 assert cursor != null;
                 int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
@@ -116,13 +137,29 @@ public class MenuActivity extends AppCompatActivity {
         } else if (requestCode == PICK_FROM_CAMERA) {
             setImage();
         }
+
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==PICK_IMAGE_REQUEST&&resultCode==RESULT_OK
+                &&data != null && data.getData() !=null){
+            filePath=data.getData();
+            try{
+                Bitmap bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+
+
+        }
+
     }
 
-    private void goToGallery() {
+    /*private void goToGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, PICK_FROM_GALLERY);
-    }
+    }*/
 
     private void takePhoto(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
