9934350f46c4ebab044e60714bf504b204207c70
eujin <kstar1996@gmail.com>
2019-04-16 20:30:09 +0900

    Delete yes or no dialog

--- app/src/main/java/com/example/eujin/real_closet/DeleteDialogActivity.java
+++ app/src/main/java/com/example/eujin/real_closet/DeleteDialogActivity.java
@@ -1,17 +1,24 @@
 package com.example.eujin.real_closet;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.widget.TextView;
 
 public class DeleteDialogActivity extends AppCompatDialogFragment {
+
+    private DialogListener listener;
+
+
+
 
     @Override
     public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Attention!")
-                .setMessage("Delete?")
+        builder.setTitle("Delete?")
+                .setMessage("Are you sure you want to delete?")
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -21,9 +28,29 @@ public class DeleteDialogActivity extends AppCompatDialogFragment {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        listener.onYesClicked();
                     }
                 });
         return builder.create();
+    }
+
+    public interface DialogListener {
+        void onYesClicked();
+
+
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (DialogListener) context;
+
+        }catch (ClassCastException e){
+            throw new ClassCastException(context.toString()
+                    +"must implement DialogListener");
+        }
     }
 }
--- app/src/main/java/com/example/eujin/real_closet/ImagesActivity.java
+++ app/src/main/java/com/example/eujin/real_closet/ImagesActivity.java
@@ -1,5 +1,6 @@
 package com.example.eujin.real_closet;
 
+import android.app.Dialog;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,7 +23,7 @@ import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ImagesActivity extends AppCompatActivity implements ImageAdapter.OnItemClickListener{
+public class ImagesActivity extends AppCompatActivity implements ImageAdapter.OnItemClickListener, DeleteDialogActivity.DialogListener {
 
     private RecyclerView mRecyclerView;
     private ImageAdapter mAdapter;
@@ -41,32 +42,32 @@ public class ImagesActivity extends AppCompatActivity implements ImageAdapter.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
 
-        mRecyclerView=findViewById(R.id.recycler_view);
+        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
 
-        mProgressCircle=findViewById(R.id.progress_circle);
-        mUploads=new ArrayList<>();//initialize as arraylist
+        mProgressCircle = findViewById(R.id.progress_circle);
+        mUploads = new ArrayList<>();//initialize as arraylist
 
-        mAdapter=new ImageAdapter(ImagesActivity.this, mUploads);
+        mAdapter = new ImageAdapter(ImagesActivity.this, mUploads);
 
         mRecyclerView.setAdapter(mAdapter);
 
         mAdapter.setOnItemClickListener(ImagesActivity.this);
 
-        mStorage=FirebaseStorage.getInstance();
+        mStorage = FirebaseStorage.getInstance();
 
-        mDatabaseRef= FirebaseDatabase.getInstance().getReference("uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
 
-        mDBListener=mDatabaseRef.addValueEventListener(new ValueEventListener() {
+        mDBListener = mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 mUploads.clear();
 
-                for (DataSnapshot postSnapshot:dataSnapshot.getChildren()){
-                    UploadActivity uploadActivity=postSnapshot.getValue(UploadActivity.class);
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    UploadActivity uploadActivity = postSnapshot.getValue(UploadActivity.class);
 
                     uploadActivity.setKey(postSnapshot.getKey());
 
@@ -82,7 +83,7 @@ public class ImagesActivity extends AppCompatActivity implements ImageAdapter.On
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(ImagesActivity.this, databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(ImagesActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
                 mProgressCircle.setVisibility(View.INVISIBLE);
             }
         });
@@ -91,14 +92,13 @@ public class ImagesActivity extends AppCompatActivity implements ImageAdapter.On
 
     @Override
     public void onItemClick(int position) {
-        Toast.makeText(this, "Normal click at position: "+position,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Normal click at position: " + position, Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onInfoClick(int position) {
-        Toast.makeText(this, "Info click at position: "+position,Toast.LENGTH_SHORT).show();//text at whatever click
-
+        Toast.makeText(this, "Info click at position: " + position, Toast.LENGTH_SHORT).show();//text at whatever click
 
 
     }
@@ -106,24 +106,42 @@ public class ImagesActivity extends AppCompatActivity implements ImageAdapter.On
     @Override
     public void onDeleteClick(int position) {
 
-        UploadActivity selectedItem=mUploads.get(position);
-        final String selectedKey=selectedItem.getKey();
+        openDialog(); //dialog activity before actually deleting picture
 
 
-        StorageReference imageRef=mStorage.getReferenceFromUrl(selectedItem.getImageUrl());
+        UploadActivity selectedItem = mUploads.get(position);
+        final String selectedKey = selectedItem.getKey();
+
+        StorageReference imageRef = mStorage.getReferenceFromUrl(selectedItem.getImageUrl());
         imageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 mDatabaseRef.child(selectedKey).removeValue();
                 Toast.makeText(ImagesActivity.this, "Item deleted", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+
     }
+
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mDatabaseRef.removeEventListener(mDBListener);
+    }
+
+    public void openDialog(){
+        DeleteDialogActivity dialog= new DeleteDialogActivity();
+        dialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+
+    @Override
+    public void onYesClicked(){
+        Toast.makeText(this, "Yes clicked: ", Toast.LENGTH_SHORT).show();
+
     }
 }
