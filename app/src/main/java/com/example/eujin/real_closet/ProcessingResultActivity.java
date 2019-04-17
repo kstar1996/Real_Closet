8fa3a44e9e4f45eeb21047ee6a8aac4d3ff84646
eujin <kstar1996@gmail.com>
2019-04-18 03:04:09 +0900

    remove opencv library and add cutout function

--- app/src/main/java/com/example/eujin/real_closet/ProcessingResultActivity.java
+++ app/src/main/java/com/example/eujin/real_closet/ProcessingResultActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.github.gabrielbb.cutout.CutOut;
+
 public class ProcessingResultActivity extends AppCompatActivity {
 
     private Button mButtonOK;
@@ -23,6 +25,8 @@ public class ProcessingResultActivity extends AppCompatActivity {
         mButtonOK = findViewById(R.id.btnOK);
 
         mOriginalImageUri = Uri.parse(getIntent().getStringExtra("uri"));
+
+        CutOut.activity().start(this);
 
         mButtonOK.setOnClickListener(new View.OnClickListener() {
             @Override
